f3d0021fcf00b8b5e50ae9a090819ab42952ff58
rlxlxl <slavavik1337@mail.ru>
2025-03-07 09:57:44 +0700

    комментарии

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,12 +1,38 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void pl_pr();
+void per_pr();
+void diag_pr();
+int main() {
+    cout << "Чтобы вы хотели вычислить:" << endl;
+    cout << "Площадь(1)" << endl;
+    cout << "Периметр(2)" << endl;
+    cout << "Длину диагонали(3)" << endl;
+    int x2;
+    cin >> x2;
+    if (x2 == 1)
+    {
+        pl_pr();
+    }
+    else if (x2 == 2)
+    {
+        per_pr();
+    }
+    else if (x2 == 3)
+    {
+        diag_pr();
+    }
+}
 void pl_pr() {
     int st1;
     int st2;
-    cout << "Введите значение двух сторон прямоугольника:" << endl;
+    cout << "Введите значение двух сторон прямоугольника:" << endl; //комментарий
     cin >> st1;
     cin >> st2;
     if (st1 > 0 & st2 > 0)
     {
-        cout << "Площадь равна " << st1 * st2 << endl;
+        cout << "Площадь равна " << st1 * st2 << endl; // комментарий 2
     }
     else
     {
@@ -45,4 +71,3 @@ void diag_pr() {
         cout << "Сторона фигуры не может быть отрицательной" << endl;
     }
 }
-
